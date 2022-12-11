80e069c7d82109acd8e6d7547cdc29779e8cb198
abdoulh <abderrahmenlouhichi3@gmail.com>
2022-12-11 23:00:19 +0100

    PHP-POST

--- index.php
+++ index.php
@@ -235,12 +235,20 @@ $birthdays['David'] = '1983-09-09';
  
  //htmlspecialchars($name, ENT_QUOTES, 'UTF-8')
 
- $firstName = $_GET['firstname'];
-$lastName = $_GET['lastname'];
+ //$firstName = $_GET['firstname'];
+//$lastName = $_GET['lastname'];
+//echo 'Welcome to our website, ' .
+ //htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
+ //htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
+
+ 
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
 echo 'Welcome to our website, ' .
- htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
- htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
-?>
+ htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' .
+ htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
+
+
 
 
  
