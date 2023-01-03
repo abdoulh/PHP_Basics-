a9dafbf61644db471fb93fd33e63dc7b08b16679
abdoulh <abderrahmenlouhichi3@gmail.com>
2023-01-03 13:06:50 +0100

    templatephp

---  count.html.php
+++  count.html.php
@@ -9,8 +9,27 @@
  
  <p>
  <?php echo $output;
+  if (!isset($_POST['firstname'])) {
+    include __DIR__ . '/../templates/form.html.php';
+ } else {
+     $firstName = $_POST['firstname'];
+     $lastName = $_POST['lastname'];
+     if ($firstName == 'Kevin' && $lastName == 'Yank') {
+         $output = 'Welcome, oh glorious leader!';
+     } else {
+         $output = 'Welcome to our website, ' .
+             htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
+             htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
+     }
+ }
+ 
+   
  ?>
  </p>
+ <p>
+ <?php echo $output; ?>
+ </p>
+
  
  </body>
 </html>
